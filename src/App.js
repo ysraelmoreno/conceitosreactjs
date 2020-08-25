af8aa187679470afce0599a2664133b6ae40e200
ysraelmoreno <ysraelmoreno02@gmail.com>
2020-08-25 05:24:07 -0300

    Added Functions of Delete and Add repositories (Static Info). All tests working.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      title : 'Desafio Node Rocket',
+      url: 'https://github.com/ysraelmoreno/desafionoderocket',
+      techs: ['NodeJS', 'JavaScript'],
+    });
+
+    setRepositories([...repositories, response.data]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
-  }
+    // Estamos chamando a função para o nosso "backend" para deletar o nosso repositório
+    await api.delete(`/repositories/${id}`);
 
+    /* Precisamos atualizar no nosso front-end, 
+    *  com as novas informações contidas dentro da API, tendo em vista que deletamos um projeto.
+    */
+    setRepositories(repositories.filter(
+      repository => repository.id != id
+    ));
+  }
+  
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => (
+        <li key={repository.id}> 
+          {repository.title} 
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+        </li>))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
